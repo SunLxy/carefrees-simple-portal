17b0348855a1555e017bf71928ef3a94605eca08
SunLxy <1011771396@qq.com>
2023-10-16 07:56:15 +0800

    feat:添加标签属性

--- core/src/utils.ts
+++ core/src/utils.ts
@@ -1,17 +1,26 @@
 import { cloneElement, isValidElement } from 'react';
-import { SimplePortal } from '.';
+import { SimplePortal } from './portal';
 
 const OpenRootDom: Map<string, SimplePortal> = new Map([]);
 
 export interface OpenSimplePortalOption {
+  /**根节点id*/
   rootId?: string;
   children: React.ReactNode;
   isOnly?: boolean;
   timeout?: number;
+  /**根节点className*/
+  rootClassName?: string
+  /**节点className*/
+  className?: string;
+  /**根节点样式*/
+  rootStyles?: string
+  /**节点样式*/
+  style?: string
 }
 
 export const openSimplePortal = (options: OpenSimplePortalOption) => {
-  const { rootId, children, isOnly = true, timeout = 0 } = options;
+  const { rootId, children, isOnly = true, timeout = 0, ...rest } = options;
   let rootDOM = OpenRootDom.get(rootId);
   /**仅展示一个节点*/
   if (!isOnly) {
@@ -26,6 +35,7 @@ export const openSimplePortal = (options: OpenSimplePortalOption) => {
     rootDOM?.hide();
   };
   rootDOM.setOptions({
+    ...rest,
     id: rootId,
     children: isValidElement(children) ? cloneElement(children, { onRequestClose } as any) : children,
     timeout,
